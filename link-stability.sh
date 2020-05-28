3d7f85356f965f44e870d93a4da0f55e33263e64
Erick Brown <ejtbpublic@gmail.com>
2020-05-28 11:23:57 -0500

    Switched from stupid to non-stupid averaging

--- link-stability.sh
+++ link-stability.sh
@@ -17,6 +17,14 @@ if [[ -z "${TIMEOUT}" ]]; then
   TIMEOUT=1
 fi
 
+# Initialize counters
+LATENCY_COUNT=0
+LATENCY_SUM=0
+UPTIME_COUNT=0
+UPTIME_SUM=0
+DOWNTIME_COUNT=0
+DOWNTIME_SUM=0
+
 # Handle ctrl-c events
 trap ctrl_c INT
 function ctrl_c() {
@@ -25,6 +33,7 @@ function ctrl_c() {
   if [[ -n "${MIN}" ]]; then
     echo "Min ping latency: ${MIN} ms"
     echo "Max ping latency: ${MAX} ms"
+    echo "Average latency:  $((${LATENCY_SUM}/${LATENCY_COUNT})) ms"
   else
     echo "No min/max latency available"
   fi
@@ -32,7 +41,7 @@ function ctrl_c() {
   if [[ -n "${MIN_UP}" ]]; then
     echo "Min uptime: ${MIN_UP} seconds"
     echo "Max uptime: ${MAX_UP} seconds"
-    echo "Avg uptime: $(echo "(${UPS})/${UP_COUNT}" | bc) seconds"
+    echo "Avg uptime: $((${UPTIME_SUM}/${UPTIME_COUNT})) seconds"
   else
     echo "No uptime stats available"
   fi
@@ -40,7 +49,7 @@ function ctrl_c() {
   if [[ -n "${MIN_DOWN}" ]]; then
     echo "Min downtime: ${MIN_DOWN} seconds"
     echo "Max downtime: ${MAX_DOWN} seconds"
-    echo "Avg downtime: $(echo "(${DOWNS})/${DOWN_COUNT}" | bc) seconds"
+    echo "Avg uptime: $((${DOWNTIME_SUM}/${DOWNTIME_COUNT})) seconds"
   else
     echo "No downtime stats available"
   fi
@@ -58,6 +67,8 @@ while [[ true ]]; do
 
   # Update min / max latency figures
   if [[ "${PINGABLE}" == "0" ]]; then
+    LATENCY_SUM=$((${LATENCY_SUM}+${LATENCY}))
+    LATENCY_COUNT=$((${LATENCY_COUNT}+1))
     if [[ -z "${MIN}" ]]; then
       MIN=${LATENCY}
       MAX=${LATENCY}
@@ -91,16 +102,11 @@ while [[ true ]]; do
         # Since the target was pingable before the state cange, but is not
         # now, this is the end of a period of uptime
 
-        # Collect uptime intervals
-        if [[ -z "${UPS}" ]]; then
-          UPS=${INTERVAL}
-          UP_COUNT=1
-        else
-          UPS="${UPS}+${INTERVAL}"
-          UP_COUNT=$((${UP_COUNT}+1))
-        fi
+        # Collect uptime data for average
+        UPTIME_SUM=$((${UPTIME_SUM}+${INTERVAL}))
+        UPTIME_COUNT=$((${UPTIME_COUNT}+1))
 
-        # Update average uptimes
+        # Update min/max uptimes
         if [[ -z "${MIN_UP}" ]]; then
           MIN_UP=${INTERVAL}
           MAX_UP=${INTERVAL}
@@ -119,16 +125,11 @@ while [[ true ]]; do
         # Since the target was not pingable before the state change, but is
         # now, this is the end of a period of downtime
 
-        # Collect downtime intervals
-        if [[ -z "${DOWNS}" ]]; then
-          DOWNS=${INTERVAL}
-          DOWN_COUNT=1
-        else
-          DOWNS="${DOWNS}+${INTERVAL}"
-          DOWN_COUNT=$((${DOWN_COUNT}+1))
-        fi
+        # Collect uptime data for average
+        DOWMTIME_SUM=$((${DOWMTIME_SUM}+${INTERVAL}))
+        DOWMTIME_COUNT=$((${DOWMTIME_COUNT}+1))
 
-        # Update average downtimes
+        # Update min/max downtimes
         if [[ -z "${MIN_DOWN}" ]]; then
           MIN_DOWN=${INTERVAL}
           MAX_DOWN=${INTERVAL}
